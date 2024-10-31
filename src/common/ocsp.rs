21b6359f175d74c8fc1036dc031c7e4291927e05
DorianCoding <108593662+DorianCoding@users.noreply.github.com>
2024-10-31 22:27:27 +0100

    Attempt

--- src/common/ocsp.rs
+++ src/common/ocsp.rs
@@ -198,7 +198,7 @@ impl OcspExt {
                 let mut id = vec![
                     0x06, 0x09, 0x2b, 0x06, 0x01, 0x05, 0x05, 0x07, 0x30, 0x01, 0x09,
                 ];
-                id.extend(&[ASN1_NULL]);
+                id.extend(&[ASN1_NULL,0,ASN1_NULL]);
                 let len = asn1_encode_length(id.len())?;
                 v.extend(len);
                 v.extend(id);
