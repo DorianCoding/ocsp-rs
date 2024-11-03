e4c73527e452e17a73ac4759ffdb0c6be5100064
DorianCoding <108593662+DorianCoding@users.noreply.github.com>
2024-11-03 21:18:08 +0100

    Corrections

--- src/common/ocsp.rs
+++ src/common/ocsp.rs
@@ -203,7 +203,7 @@ impl OcspExt {
                 let len = asn1_encode_length(id.len())?;
                 v.extend(len);
                 v.extend(id);
-                v.extend(&[ASN1_BIT_STRING,0,ASN1_NULL]);
+                v.extend(&[ASN1_BIT_STRING]);
             }
             _ => {
                 error!("Unsupported Extension");
